e18bdedb508c5ac57067bf2f08c9b530e87e1417
MayuriBM <mimayurimi1@gmail.com>
2017-02-20 18:24:39 +0100

    Signed-off-by: MayuriBM <mimayurimi1@gmail.com>

--- src/main/java/es/salesianos/edu/webpages/HomePage.java
+++ src/main/java/es/salesianos/edu/webpages/HomePage.java
@@ -1,5 +1,15 @@
 package es.salesianos.edu.webpages;
 
-public class HomePage {
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
+public class HomePage extends WebPage {
+	
+	public HomePage(){
+		
+		Label label = new Label("holamundo",Model.of("buenos dias"));
+				add(label);
+	}
 
 }
