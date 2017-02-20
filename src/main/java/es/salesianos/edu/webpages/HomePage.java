0d3fdc1a85995f098287d06f0916dadd9079752f
MayuriBM <mimayurimi1@gmail.com>
2017-02-20 18:59:38 +0100

    cuarto Signed-off-by: MayuriBM <mimayurimi1@gmail.com>

--- src/main/java/es/salesianos/edu/webpages/HomePage.java
+++ src/main/java/es/salesianos/edu/webpages/HomePage.java
@@ -2,14 +2,17 @@ package es.salesianos.edu.webpages;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.Model;
 
 public class HomePage extends WebPage {
 	
 	public HomePage(){
 		
-		Label label = new Label("holamundo",Model.of("buenos dias"));
-		add(label);
+		BookmarkablePageLink bookmarkablePageLink1 = new BookmarkablePageLink("linkAuthorForm", AuthorPage.class);
+		BookmarkablePageLink bookmarkablePageLink2 = new BookmarkablePageLink("linkListAuthor",
+				ListAuthorPage.class);
+		add(bookmarkablePageLink1);
+		add(bookmarkablePageLink2);
 	}
-
 }
